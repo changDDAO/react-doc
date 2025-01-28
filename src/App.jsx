f93c929d5acdfaf214903929194fd82a2e00bc29
changDDAO <younch8342@naver.com>
2025-01-28 20:01:37 +0900

    feat: React 에서 list를 Array에 담아서 Rendering할 떄, key값을 들고 가야하는 이유 및 트리형 구조로 리액트를 이해할 필요

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FilterableProductTable from "./components/FilterableProductTable.jsx";
 import Profile from "./components/Profile.jsx";
 import Gallery from "./components/Gallery.jsx";
 import Bio from "./components/Bio.jsx";
+import {people} from "./components/data.js";
+import {getImageUrl} from "./components/utils.js";
 
 function App() {
     const PRODUCTS = [
@@ -15,12 +17,26 @@ function App() {
         {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
         {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
     ];
+    const chemists = people.filter((person) => person.profession === 'chemist');
+    const listItems = chemists.map((person)=>
+    <li key={person.id}>
+        <img
+            src={getImageUrl(person)}
+            alt={person.name}
+        />
+        <p>
+            <b>{person.name}</b>
+            {` ${person.profession} known for ${person.accomplishment}`}
+        </p>
+    </li>
+    )
 
     return (
         <>
             {/*<FilterableProductTable products={PRODUCTS}/>*/}
             {/*<Gallery/>*/}
-            <Bio/>
+            {/*<Bio/>*/}
+            <ul>{listItems}</ul>
         </>
     );
 
